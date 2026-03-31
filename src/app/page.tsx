610910ddb637ee4d275bbc8e71ee3c86443b8fd3
HebaElgohary <hebasamir535@gmail.com>
2026-03-31 22:08:58 +0200

    feat(home):add footerSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/organisms/Footer";
 import HeroSection from "@/components/organisms/HeroSection";
 import Navbar from "@/components/organisms/Navbar";
 
@@ -6,6 +7,7 @@ export default function page() {
     <div>
       <Navbar />
       <HeroSection />
+      <Footer />
      
     </div>
   );
---
+++ src/components/organisms/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-red-300 !p-5 flex justify-center text-white items-center text-3xl'>Footer</div>
+  )
+}
