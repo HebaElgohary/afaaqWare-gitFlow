e9997d0755adee71605ef48a60d083f48bdb8def
HebaElgohary <hebasamir535@gmail.com>
2026-03-31 22:01:11 +0200

    feat(home):add heroSection

---
+++ src/components/organisms/HeroSection.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className='bg-red-100 flex justify-center items-center min-h-[80vh] text-3xl' >HeroSection</div>
+  )
+}
